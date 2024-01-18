fb784d40b831a6e424ec36af546c7d26f38a35e3
braim23 <94292623+braim23@users.noreply.github.com>
2024-01-18 03:36:36 +0300

    adding functionality for live videos

--- src/components/extractVideoId.js
+++ src/components/extractVideoId.js
@@ -5,6 +5,9 @@ export function extractVideoId(youtubeLink) {
   // Regular expression to match the video ID after the last '/' in 'youtu.be' links
   const shortRegex = /youtu\.be\/([^/?]+)/;
 
+  // Regular expression to match the video ID after '/live/' in live video links
+  const liveRegex = /\/live\/([^/?]+)/;
+
   // Check for a match in regular YouTube links
   const regularMatch = youtubeLink.match(regularRegex);
 
@@ -21,7 +24,15 @@ export function extractVideoId(youtubeLink) {
     return shortMatch[1];
   }
 
-  // Handle the case when no match is found for both regular and 'youtu.be' links
+  // Check for a match in live video links
+  const liveMatch = youtubeLink.match(liveRegex);
+
+  if (liveMatch && liveMatch[1]) {
+    // Return the extracted video ID from live video links
+    return liveMatch[1];
+  }
+
+  // Handle the case when no match is found for regular, 'youtu.be', and live video links
   console.error("Invalid YouTube link");
   return null;
 }
