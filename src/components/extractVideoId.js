720fe6ef0f81da6041f2ba392668b9b3c075b6f3
braim23 <94292623+braim23@users.noreply.github.com>
2024-01-18 03:26:34 +0300

    altering the extractVideoId function

--- src/components/extractVideoId.js
+++ src/components/extractVideoId.js
@@ -1,15 +1,27 @@
 export function extractVideoId(youtubeLink) {
-  // Regular expression to match the video ID after 'v='
-  const regex = /[?&]v=([^&]+)/;
-  const match = youtubeLink.match(regex);
+  // Regular expression to match the video ID after 'v=' in regular YouTube links
+  const regularRegex = /[?&]v=([^&]+)/;
 
-  // Check if a match is found
-  if (match && match[1]) {
-    // Return the extracted video ID
-    return match[1];
-  } else {
-    // Handle the case when no match is found
-    console.error("Invalid YouTube link");
-    return null;
+  // Regular expression to match the video ID after the last '/' in 'youtu.be' links
+  const shortRegex = /youtu\.be\/([^/?]+)/;
+
+  // Check for a match in regular YouTube links
+  const regularMatch = youtubeLink.match(regularRegex);
+
+  if (regularMatch && regularMatch[1]) {
+    // Return the extracted video ID from regular YouTube links
+    return regularMatch[1];
   }
+
+  // Check for a match in 'youtu.be' links
+  const shortMatch = youtubeLink.match(shortRegex);
+
+  if (shortMatch && shortMatch[1]) {
+    // Return the extracted video ID from 'youtu.be' links
+    return shortMatch[1];
+  }
+
+  // Handle the case when no match is found for both regular and 'youtu.be' links
+  console.error("Invalid YouTube link");
+  return null;
 }
